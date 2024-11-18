a46a3ad90e713b07ba0bbadc959cbe997cb24b2c
Princess <wtc@pop-os.localdomain>
2024-11-18 15:56:25 +0200

    added a function to calculate celsius and fahrenheit

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = Flask(__name__, template_folder="templates")
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "baab4fef1af1a796ef7429d969971b73"
+
+def temperature_to_celsius_fahrenheit(temperature):
+    celsius = temperature - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
