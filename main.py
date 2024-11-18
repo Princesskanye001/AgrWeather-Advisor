e91be195e4fdb649fca6b937a26b9af8aa6e2169
Princess <wtc@pop-os.localdomain>
2024-11-18 16:02:41 +0200

    elif statement for < 32 temp

--- main.py
+++ main.py
@@ -66,4 +66,10 @@ def index():
                                 3)Squash: Squash plants prefer warm soil and should be planted after the soil has warmed to at least 18°C.
                                 4)Melons: Melon seeds should be planted in warm soil, typically when temperatures are consistently above 18°C
                                 """
-                    weather_image = "static/download.jpg"        
+                    weather_image = "static/download.jpg"
+                elif temp_celsius >= 21 and temp_celsius < 32:
+                    message= """With the temperature of your city, here are plants you can sow:
+                                1)Peppers
+                                2)Watermelons
+                                3)Okra"""
+                    weather_image = "static/Fairy World.jpg"            
