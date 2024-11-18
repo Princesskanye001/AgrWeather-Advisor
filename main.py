9ff20398962ddb9b9a2a13b50dd53e0fca3a3afd
Princess <wtc@pop-os.localdomain>
2024-11-18 15:57:39 +0200

    added GET and POST methods

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ def temperature_to_celsius_fahrenheit(temperature):
     celsius = temperature - 273.15
     fahrenheit = celsius * (9/5) + 32
     return celsius, fahrenheit
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    error_message = None
+    weather_image = None
+    if request.method == "POST":
+        city = request.form["city"]
+        url = BASE_URL + "appid=" + API_KEY + "&q=" + city
+        while True:
+            response = requests.get(url)
+            if response.status_code ==200:
+                data = response.json()
+                temp = data['main']['temp']
+                temp_celsius, temp_fahrenheit = temperature_to_celsius_fahrenheit(temp)
+                feels_like_temp = data['main']['feels_like']
+                feels_like_celsius, feels_like_fahrenheit = temperature_to_celsius_fahrenheit(feels_like_temp)
+                wind_speed = data['wind']['speed']
+                humidity = data['main']['humidity']
+                description = data['weather'][0]['description']
+                sunrise_time = dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+                sunset_time = dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
