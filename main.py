d5a9f540e03fd20b3c1689cb49b212f13808786e
Princess <wtc@pop-os.localdomain>
2024-11-18 16:25:08 +0200

    fixing cs

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = Flask(__name__, template_folder="templates")
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "baab4fef1af1a796ef7429d969971b73"
 
+
+
 def temperature_to_celsius_fahrenheit(temperature):
     celsius = temperature - 273.15
     fahrenheit = celsius * (9/5) + 32
@@ -33,14 +35,14 @@ def index():
                 sunrise_time = dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
                 sunset_time = dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
 
+            
                 if temp_celsius < 5:
                     message = """With the temperature of your city, here are the plants you can sow: 
                                     1)Garlic: Garlic cloves can be planted in cool soil in late fall for
                                     overwintering or early spring for spring harvest.
                                     2)Onions: Onions can tolerate cool soil temperatures and are often planted early in the spring.
                                     3)winter wheat"""
-                    weather_image = "images/cloud(2).jpg"
-
+                    weather_image = "static/cloud(2).jpg"
                 elif 5 <= temp_celsius < 10:
                     message = """With the temperature of your city, here are plants you can sow:
                                 1)Carrots: Carrots can be sown in cool soil and will germinate at temperatures as low as 5°C.
@@ -50,7 +52,7 @@ def index():
                                 5)beets
                                 6)radishes
                                 7)turnips"""
-                    weather_image = "images/cloud(1).jpg"
+                    weather_image = "static/cloud(1).jpg"
                 elif 10 <= temp_celsius < 17:
                     message="""With the temperature of your city, here are plants you can sow:
                                 1)Beans: Beans prefer warmer soil temperatures and should be planted when the soil has warmed to at least 10°C.
@@ -58,7 +60,8 @@ def index():
                                 3)Tomatoes: Tomato plants thrive in warm soil and should be planted after the soil has reached 10-18°C.
                                 4)Herbs (e.g. parsley, cilantro)
                                 5)spring wheat"""
-                    weather_image = "images/download.jpg"
+                    weather_image = "static/download.jpg"
+        
                 elif 17 <= temp_celsius < 21:
                     message ="""With the temperature of your city, here are plants you can sow:
                                 1)Herbs: e.g Some herbs like basil prefer warmer temperatures, around 18°C (64°F) or higher.
@@ -66,14 +69,14 @@ def index():
                                 3)Squash: Squash plants prefer warm soil and should be planted after the soil has warmed to at least 18°C.
                                 4)Melons: Melon seeds should be planted in warm soil, typically when temperatures are consistently above 18°C
                                 """
-                    weather_image = "images/download.jpg"
+                    weather_image = "static/download.jpg"
                 elif temp_celsius >= 21 and temp_celsius < 32:
                     message= """With the temperature of your city, here are plants you can sow:
                                 1)Peppers
                                 2)Watermelons
                                 3)Okra"""
-                    weather_image = "images/Fairy World.jpg"
-
+                    weather_image = "static/Fairy World.jpg"
+                    
                 else:
                         message = """Its really hot in your city,remember even heat-tolerant plants will require adequatvwater to establish themselves during hot weather.
                             These are the plants that can survive hot temperatures:
@@ -90,18 +93,17 @@ def index():
                             8)Drought-Resistant Plants: Plants that are naturally adapted to arid climates or have deep root 
                                 systems to access water can also be sown in hot conditions. Examples include cacti, succulents, 
                                 and some varieties of ornamental grasses."""
-                        weather_image = "images/Fairy World.jpg"
+                        weather_image = "static/Fairy World.jpg"
 
                 return render_template("index.html", city=city, temp_celsius=temp_celsius, temp_fahrenheit=temp_fahrenheit,
                                     feels_like_celsius=feels_like_celsius, feels_like_fahrenheit=feels_like_fahrenheit,
                                     humidity=humidity, description=description, sunrise_time=sunrise_time, sunset_time=sunset_time,message = message,weather_image=weather_image)
+            
             else:
                 error_message = "Invalid city. Check your spelling and try again."    
             return render_template("index.html", error_message = error_message)
-    return render_template("index.html", error_message=error_message) 
+        
+    return render_template("index.html", error_message=error_message)    
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-                                
